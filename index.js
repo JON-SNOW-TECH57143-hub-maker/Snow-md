ca84e4e3671859ade3e496cb87411f00fb2cda13
JON-SNOW-TECH57143-hub <skyprimejockerx@gmail.com>
2026-05-11 14:48:15 -0400

    Update index.js

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ const { Boom } = require('@hapi/boom');
 const { MongoClient } = require('mongodb');
 
 const MONGO_URI = process.env.MONGO_URI || 'mongodb://localhost:27017';
-const DB_NAME   = 'snowmd_bot';
+const DB_NAME   = 'Snow-md';
 let   mongoDb   = null;
 
 async function connectMongo() {
